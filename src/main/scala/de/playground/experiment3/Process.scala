8aebea87d0e7a295962d11fc1fa7f9a955cb3def
Marco Wittig <marco.wittig@jambit.com>
2020-07-10 21:52:59 +0200

    Create experiment 3

---
+++ src/main/scala/de/playground/experiment3/Process.scala
@@ -0,0 +1,12 @@
+package de.playground.experiment3
+
+object Process {
+
+  def doSomething(item1: Box, item2: Box) = {
+    (item1, item2) match {
+      case (Value(v1: Int), Value(v2: Int)) => Value(v1 + v2)
+      case (Value(v1: String), Value(v2: String)) => Value(v1 + v2)
+      case (Value(v1: Double), Value(v2: Double)) => Value(v1 + v2)
+    }
+  }
+}
